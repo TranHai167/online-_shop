d1d42029c2804fd6127b8669c72e3322d1717ea5
huythoi123 <luuhieuhuy1005@gmail.com>
2023-08-13 14:34:09 +0700

    change to getAllProduct

--- src/main/java/com/example/productmanagmentmodule/controller/ProductController.java
+++ src/main/java/com/example/productmanagmentmodule/controller/ProductController.java
@@ -16,7 +16,7 @@ public class ProductController {
 
     @GetMapping("/getAll")
     public ResponseEntity<ProductsResponse[]> getCategories() {
-        return ResponseEntity.ok(categoryService.getAllCategories());
+        return ResponseEntity.ok(categoryService.getAllProduct());
     }
 
     @GetMapping("/get")
--- src/main/java/com/example/productmanagmentmodule/service/ProductService.java
+++ src/main/java/com/example/productmanagmentmodule/service/ProductService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    ProductsResponse[] getAllCategories();
+    ProductsResponse[] getAllProduct();
 
     ProductsResponse getProductById(String id);
 }
