cb7d1f2787c369b3ce7ae6ae17d262a4c563d00c
huythoi123 <luuhieuhuy1005@gmail.com>
2023-08-14 11:11:46 +0700

    fix problems involve quantity in ShoppingCartServiceImpl.java

--- src/main/java/com/example/productmanagmentmodule/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/example/productmanagmentmodule/service/impl/ShoppingCartServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.productmanagmentmodule.service.impl;
 
+import com.example.productmanagmentmodule.entity.Products;
+import com.example.productmanagmentmodule.entity.ShoppingCart;
+import com.example.productmanagmentmodule.entity.ShoppingCartItem;
 import com.example.productmanagmentmodule.model.request.AddShoppingCartInfoRequest;
 import com.example.productmanagmentmodule.repository.ShoppingCartRepository;
 import com.example.productmanagmentmodule.service.ShoppingCartService;
+import com.example.productmanagmentmodule.exception.CommonException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,66 +15,58 @@ import org.springframework.stereotype.Service;
 public class ShoppingCartServiceImpl implements ShoppingCartService {
     private final ShoppingCartRepository shoppingCartRepository;
 
-//    @Override
-//    public String createShoppingCart(AddShoppingCartInfoRequest request) {
-//        String cartId = request.getCartId();
-//        Product product = request.getProduct();
-//
-//        // logic to save to db ShoppingCartRepository
-//        ShoppingCart shoppingCart = new ShoppingCart();
-//        shoppingCart.setCartId(cartId);
-//        shoppingCart.setQuantity(request.getQuantity());
-//        shoppingCart.setProductId(product.getId());
-//
-//        shoppingCartRepository.save(shoppingCart);
-//        return cartId;
-//    }
-
     @Override
     public String createShoppingCart(AddShoppingCartInfoRequest request) {
-        return null;
+        String cartId = request.getCartId();
+        Products product = request.getProducts();
+
+        // logic to save to db ShoppingCartRepository
+        ShoppingCartItem[] shoppingCartItem = {new ShoppingCartItem(product, request.getQuantity())};
+
+        ShoppingCart shoppingCart = new ShoppingCart(cartId, shoppingCartItem);
+
+        shoppingCartRepository.save(shoppingCart);
+        return cartId;
     }
+
 
     @Override
     public String updateShoppingCart(AddShoppingCartInfoRequest request) {
-        return null;
+        String cartId = request.getCartId();
+        Products product = request.getProducts();
+
+        // logic to update to db ShoppingCartRepository
+        ShoppingCart updateShoppingCart = shoppingCartRepository.findById(cartId).orElse(null);
+        if (updateShoppingCart != null){
+            ShoppingCartItem[] oldShoppingCartItems = updateShoppingCart.getItems();
+            ShoppingCartItem[] newShoppingCartItems = {new ShoppingCartItem(product, oldShoppingCartItems[0].getQuantity() + 1)};
+            updateShoppingCart.setItems(newShoppingCartItems);
+            shoppingCartRepository.save(updateShoppingCart);
+            return cartId;
+        }
+        throw new CommonException("400", "shopping cart doesn't exist");
     }
 
-//    @Override
-//    public String updateShoppingCart(AddShoppingCartInfoRequest request) {
-//        String cartId = request.getCartId();
-//        Product product = request.getProduct();
-//
-//        // logic to update to db ShoppingCartRepository
-//        ShoppingCart updateShoppingCart = shoppingCartRepository.findById(cartId).orElse(null);
-//        if (updateShoppingCart != null){
-//            updateShoppingCart.setQuantity(updateShoppingCart.getQuantity() + 1);
-//            return cartId;
-//        }
-//        throw new CommonException("400", "shopping cart doesn't exist");
-//    }
 
     @Override
     public String deleteShoppingCart(AddShoppingCartInfoRequest request) {
-        return null;
-    }
+        String cartId = request.getCartId();
+        Products product = request.getProducts();
 
-//    @Override
-//    public String deleteShoppingCart(AddShoppingCartInfoRequest request) {
-//        String cartId = request.getCartId();
-//        Product product = request.getProduct();
-//
-//        // logic to update to db ShoppingCartRepository
-//        ShoppingCart updateShoppingCart = shoppingCartRepository.findById(cartId).orElse(null);
-//        if (updateShoppingCart != null){
-//            if (updateShoppingCart.getQuantity() == 1){
-//                shoppingCartRepository.delete(updateShoppingCart);
-//                return cartId;
-//            }
-//            updateShoppingCart.setQuantity(updateShoppingCart.getQuantity() - 1);
-//            return cartId;
-//        }
-//        throw new CommonException("400", "shopping cart doesn't exist");
-//    }
+        // logic to update to db ShoppingCartRepository
+        ShoppingCart updateShoppingCart = shoppingCartRepository.findById(cartId).orElse(null);
+        if (updateShoppingCart != null){
+            ShoppingCartItem[] oldShoppingCartItems = updateShoppingCart.getItems();
+            if (oldShoppingCartItems[0].getQuantity() != 1){
+                ShoppingCartItem[] newShoppingCartItems = {new ShoppingCartItem(product, oldShoppingCartItems[0].getQuantity() - 1)};
+                updateShoppingCart.setItems(newShoppingCartItems);
+                shoppingCartRepository.save(updateShoppingCart);
+                return cartId;
+            }
+            shoppingCartRepository.delete(updateShoppingCart);
+            return cartId;
+        }
+        throw new CommonException("400", "shopping cart doesn't exist");
+    }
 
 }
